5b4e5b3443ac109f734678290239438f959c4ae9
natkobylinska <natkobylinska@gmail.com>
2023-05-25 09:20:25 +0200

    fundamental challenges
    first super easy 4 ones

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,25 @@
 'use strict';
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+document.querySelector('.number').innerHTML = secretNumber;
+
+document.querySelector('.check').addEventListener('click', () => {
+  const guess = Number(document.querySelector('.guess').value);
+
+  if (!guess) {
+    document.querySelector('.number').innerHTML = ':(';
+  } else {
+    if (guess > secretNumber) {
+      document.querySelector('.message').innerHTML = 'Too big..';
+      score--;
+    } else if (guess < secretNumber) {
+      document.querySelector('.message').innerHTML = 'Too small..';
+      score--;
+    } else {
+      document.querySelector('.message').innerHTML = 'Correct!';
+    }
+  }
+  document.querySelector('.score').innerHTML = score;
+});
